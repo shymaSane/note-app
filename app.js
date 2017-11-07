53d3e2b6521f3e250b942cd6b2347ff91ba3746d
shymaSane <shymaSane1@gmail.com>
2017-11-07 17:00:50 +0200

    requiring my own files

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-console.log ("it started");
+console.log ("app.js started");
 
 
 const fs = require("fs");
-const os = require("os")
+const os = require("os");
+const notes = require("./notes.js")
 
+var res = notes.addNote()
+console.log(res)
 
- var user = os.userInfo()
+//  var user = os.userInfo()
 
- fs.appendFile("greeting.txt", `Hello ${user.username} !`);
+//  fs.appendFile("greeting.txt", `Hello ${user.username} !, and your age ${notes.age}`);
 
-//  console.log(user)
+// //  console.log(user)
---
+++ notes.js
@@ -0,0 +1,8 @@
+console.log("notes.js started!")
+
+// module.exports.age = 25;
+
+module.exports.addNote = () => {
+    console.log("worked !!");
+    return "new note"
+}
