d33d04cc18b85f80059a52cd137025ace446b150
shymaSane <shymaSane1@gmail.com>
2017-11-07 22:51:44 +0200

    working on process.argv

--- app.js
+++ app.js
@@ -2,13 +2,28 @@ console.log ("app.js started");
 
 
 const fs = require("fs");
-const os = require("os");
+// const os = require("os");
 // it should be written before we calling the functions in it (written before in order )
 const _= require ("lodash")
 const notes = require("./notes.js")
 
-console.log(_.isString(true))
-console.log(_.isString("SHYMA"))
+var command = process.argv[2];
+console.log(`command is: ${command}`);
+
+if (command === 'add'){
+    console.log("new note added");
+} else if (command === 'list'){
+    console.log("list all notes");
+} else if (command === 'read'){
+    console.log("read current note");
+} else if (command === 'delete'){
+    console.log("delete current note");
+} else{
+    console.log ("command not recongized ")
+}  
+
+
+
 
 // var res = notes.addNote()
 // console.log(res)
@@ -17,5 +32,5 @@ console.log(_.isString("SHYMA"))
 
 //  fs.appendFile("greeting.txt", `Hello ${user.username} !, and your age ${notes.age}`);
 
-// //  console.log(user)
+//  console.log(user)
  
