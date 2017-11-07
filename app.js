97a3fbc8c29910d50be8d8c2ac08269cf384673f
shymaSane <shymaSane1@gmail.com>
2017-11-07 18:42:18 +0200

    working with lodash

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ console.log ("app.js started");
 
 const fs = require("fs");
 const os = require("os");
+// it should be written before we calling the functions in it (written before in order )
+const _= require ("lodash")
 const notes = require("./notes.js")
 
-var res = notes.addNote()
-console.log(res)
+console.log(_.isString(true))
+console.log(_.isString("SHYMA"))
+
+// var res = notes.addNote()
+// console.log(res)
 
 //  var user = os.userInfo()
 
 //  fs.appendFile("greeting.txt", `Hello ${user.username} !, and your age ${notes.age}`);
 
 // //  console.log(user)
+ 
