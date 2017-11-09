f1654787d6f146a2930ec0453cd302c4282b6b5d
shymaSane <shymaSane1@gmail.com>
2017-11-09 20:58:42 +0200

    working with yargs

--- app.js
+++ app.js
@@ -5,16 +5,22 @@ const fs = require("fs");
 // const os = require("os");
 // it should be written before we calling the functions in it (written before in order )
 const _= require ("lodash")
+const yargs = require("yargs")
+
 const notes = require("./notes.js")
+const argv = yargs.argv
 
 var command = process.argv[2];
 console.log(`command is: ${command}`);
+// console.log(`process: ${process.argv}`); >> it didnt writing it that way 
+console.log("process: ", process.argv)
+console.log(" yargs:" , argv)
 
 if (command === 'add'){
-    console.log("new note added");
+    notes.addNote(argv.title, argv.body)
 } else if (command === 'list'){
     console.log("list all notes");
-} else if (command === 'read'){
+} else if (command === 'read'){ 
     console.log("read current note");
 } else if (command === 'delete'){
     console.log("delete current note");
--- notes.js
+++ notes.js
@@ -2,7 +2,11 @@ console.log("notes.js started!")
 
 // module.exports.age = 25;
 
-module.exports.addNote = () => {
-    console.log("worked !!");
-    return "new note"
+var addNote = (title, body) => {
+console.log("new note added", title, body)
+}
+
+module.exports = {
+    //if the key and the value is the same so wrtie it like this addNote: addNote
+    addNote
 }
