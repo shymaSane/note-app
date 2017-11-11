bbb5c0089f5eeb9d26128e163914e67a726923d7
shymaSane <shymaSane1@gmail.com>
2017-11-11 13:19:50 +0200

    working with JSON

--- testing/json.js
+++ testing/json.js
@@ -1,14 +1,34 @@
-var obj = {
-    name: "shyma",
-    age: 25
+// var obj = {
+//     name: "shyma",
+//     age: 25
+// }
+// // from object to string:
+// var person = JSON.stringify(obj)
+
+// console.log(typeof person)
+// console.log(person)
+// // from string to object:
+// var person1 = JSON.parse(person)
+
+// console.log(typeof person1)
+// console.log(person1)
+
+
+var originalNote = {
+    title: "secret",
+    body: "my little secret"
 }
+// sending object to store it
+const fs = require('fs')
 
-var person = JSON.stringify(obj)
+var originalNoteString = JSON.stringify(originalNote);
 
-console.log(typeof person)
-console.log(person)
+fs.writeFileSync('notes.json', originalNoteString)
 
-var person1 = JSON.parse(person)
+// bring the object back 
 
-console.log(typeof person1)
-console.log(person1)
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(note)
+console.log(note.title)
