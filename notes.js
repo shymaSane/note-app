c63c92d18b3fce31c587d0b9e2eb499c71813132
shymaSane <shymaSane1@gmail.com>
2017-11-13 14:47:10 +0200

    fetchNotes fixed

--- notes.js
+++ notes.js
@@ -3,10 +3,22 @@ console.log("notes.js started!")
 // module.exports.age = 25;
 const fs = require('fs')
 
+// since we have o use these functions in more than on methods so it ll be easier to 
+// make a different function for it rather than repeat it remember DRY:
+
+var fetchNotes = () => {
+ try{
+    var noteString = fs.readFileSync('notes-data.json')
+    return JSON.parse(noteString)
+ } catch(e) {
+     return [];
+ }
+}
+
 // adding and saving notes:
 
 var addNote = (title, body) => {
-var notes = [];
+var notes = fetchNotes();
 var note = {
     title,
     body
@@ -15,12 +27,12 @@ var note = {
 // if we deleted the file data-notes then we ll have problem saving data and error ll accures so
 // we ll add try
 
-try{
-    var noteString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(noteString);
-} catch(e){
+// try{
+//     var noteString = fs.readFileSync('notes-data.json');
+//     notes = JSON.parse(noteString);
+// } catch(e){
 
-}
+// }
 
 //we dont want to save the same title over the one time so we shld make function:
  var duplicate = notes.filter((note) => note.title === title);
